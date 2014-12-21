7660b137021a91bd5476ea38622ad418a5839daf
Robbyn Rahmandaru <robzlabz@gmail.com>
2014-12-21 16:02:26 +0700

    First Commit

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ Easy-script
 ===========
 
 Make Life Simply Easier
+
+## / Vesta with MariaDB
+
+- Replace MySQL with MariaDB
+- Remove Backup Cron
+
+## / Wordpess Install
+- Easy Console Install Wordpress
+
---
+++ wordpress-vesta/wp.sh
@@ -0,0 +1,19 @@
+wget https://wordpress.org/latest.zip
+unzip latest.zip
+cd wordpress
+mv * ../
+cd ..
+rm latest.zip
+rmdir wordpress
+read -p "Database Name : " dbname
+read -p "Database User : " dbuser
+read -p "Database Password : " dbpass
+sed "s/database_name_here/$dbname/g" wp-config-sample.php > wp-config1.php
+rm wp-config-sample.php
+sed "s/username_here/$dbuser/g" wp-config1.php > wp-config2.php
+rm wp-config1.php
+sed "s/password_here/$dbpass/g" wp-config2.php > wp-config.php
+rm wp-config2.php
+echo '=============='
+echo 'Setup Complete'
+echo '=============='
