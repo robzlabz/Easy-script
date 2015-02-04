ff8cce4f1ca3df99d03a8b4fd5549772e0d3df10
Robbyn Rahmandaru <robzlabz@gmail.com>
2015-02-04 11:14:23 +0700

    phalcon install

---
+++ vestacp-phalcon/centos/phalcon.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Created by Robbyn
+# Bash Install Phalcon for Centos 6.x VestaCP
+
+read -p "Downgrade PHP (y/n) [y] : " downgrade
+if [ ! "$downgrade" = 'n']; then
+	# Downgrade PHP to 5.3
+	/usr/bin/yum downgrade php*
+fi
+
+# Install Phalcon Requirement
+/usr/bin/yum install -y php-devel php-mysql gcc libtool
+/usr/bin/yum install -y pcre pcre-devel
+/usr/bin/yum install -y git
+
+# Install Phalcon
+git clone --depth=1 git://github.com/phalcon/cphalcon.git
+cd cphalcon/build
+sudo ./install
+
+# Make Phalcon Run
+echo "extension=phalcon.so" >> /etc/php.d/pdo_mysql.ini
+
+# Install Phalcon Development Tools
+mkdir /usr/local/phalcon && cd /usr/local/phalcon
+curl -s http://getcomposer.org/installer | php
+echo '{' >> composer.json
+echo '    "require": {' >> composer.json
+echo '        "phalcon/devtools": "dev-master"' >> composer.json
+echo '    }' >> composer.json
+echo '}' >> composer.json
+php composer.phar install
+ln -s /usr/local/phalcon/vendor/phalcon/devtools/phalcon.php /usr/bin/phalcon
+chmod ugo+x /usr/bin/phalcon
+
+# Make phpinfo() test
+echo "<?php phpinfo(); ?>" > /home/admin/web/default.domain/public_html/info.php
+
+# Create Result
+myip=$(wget vestacp.com/what-is-my-ip/ -O - 2>/dev/null)
+echo ""
+echo "Browse to ${myip}/info.php to make sure phalcon is running. Thanks"
